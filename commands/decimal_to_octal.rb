937688c271e5ea82b476585ea44981bf798f07c4
sgtcoolguy <cwilliams@aptana.com>
2010-01-21 11:42:58 -0500

    use pure ruby to do decimal/hex/octal conversions

--- commands/decimal_to_octal.rb
+++ commands/decimal_to_octal.rb
@@ -4,7 +4,5 @@ command 'Decimal to Octal' do |cmd|
   cmd.key_binding = 'CONTROL+M2+C'
   cmd.output = :replace_selection
   cmd.input = :selection, :word
-  cmd.invoke =<<-EOF
-bc <<<"obase=8;$(cat)"|tr -d \\n
-EOF
+  cmd.invoke { STDIN.read.to_i.to_s(8) }
 end
--- commands/hex_to_decimal.rb
+++ commands/hex_to_decimal.rb
@@ -4,7 +4,5 @@ command 'Hex to Decimal' do |cmd|
   cmd.key_binding = 'CONTROL+M2+C'
   cmd.output = :replace_selection
   cmd.input = :selection, :word
-  cmd.invoke =<<-EOF
-bc <<<"ibase=16;$(tr '[a-z]' '[A-Z]')"|tr -d \\n
-EOF
+  cmd.invoke { STDIN.read.hex }
 end
