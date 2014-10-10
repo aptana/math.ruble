4f35db0357b8a951d258872ce9585131da6f848e
sgtcoolguy <chris.a.williams@gmail.com>
2014-10-10 15:47:35 -0400

    Fix UTF-8 strings, set encoding

--- commands/subtract_selected_numbers.rb
+++ commands/subtract_selected_numbers.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'ruble'
 
 command t(:subtract_numbers) do |cmd|
@@ -5,7 +6,7 @@ command t(:subtract_numbers) do |cmd|
   cmd.output = :insert_as_text
   cmd.input = :selection, :line
   cmd.invoke do
-    numbers = $stdin.read.gsub(/[$ۣ]/, '').scan(/((?:\b|-)[0-9]+(?:\.[0-9]+)?)\b/)
+    numbers = $stdin.read.gsub(/[$€£]/, '').scan(/((?:\b|-)[0-9]+(?:\.[0-9]+)?)\b/)
     print " = " + numbers[1..-1].inject(numbers.first.first.to_f) { |t,s| t - s[0].to_f() }.to_s.sub(/.0+\z/, '')
   end
 end
