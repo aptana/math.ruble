b922bf4230eeb39e776ac9c05ba4dc163119f12f
sgtcoolguy <chris.a.williams@gmail.com>
2014-10-10 15:47:35 -0400

    Fix UTF-8 strings, set encoding

--- commands/add_selected_numbers.rb
+++ commands/add_selected_numbers.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'ruble'
 
 command t(:add_numbers) do |cmd|
@@ -5,6 +6,6 @@ command t(:add_numbers) do |cmd|
   cmd.output = :insert_as_text
   cmd.input = :selection, :line
   cmd.invoke do
-    print " = " + $stdin.read.gsub(/[$ۣ]/, '').scan(/((?:\b|-)[0-9]+(?:\.[0-9]+)?)\b/).inject(0.0) { |t,s| t + s[0].to_f() }.to_s.sub(/.0+\z/, '')
+    print " = " + $stdin.read.gsub(/[$€£]/, '').scan(/((?:\b|-)[0-9]+(?:\.[0-9]+)?)\b/).inject(0.0) { |t,s| t + s[0].to_f() }.to_s.sub(/.0+\z/, '')
   end
 end
